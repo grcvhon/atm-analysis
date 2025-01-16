5ca56eba6357d8e1872be5d2148241f57283e166
Vhon Garcia <81279614+grcvhon@users.noreply.github.com>
2025-01-16 14:41:11 +1030

    update script

--- script/test_script.R
+++ script/test_script.R
@@ -1,7 +1,22 @@
 ## MaxEnt modelling framework ##
 ## 2025-01-16 ##
 
-# Load packages
+#### Install packages ####
+# install.packages("sf")
+# install.packages("leaflet")
+# install.packages("readr)
+# install.packages("janitor")
+# install.packages("dplyr")
+# install.packages("mapview")
+# install.packages("spatstat")
+# install.packages("tidyverse")
+# install.packages("raster")
+# install.packages("dismo")
+# install.packages("lubridate")
+# install.packages("SDMtune")
+
+
+#### Load packages ####
 library(sf)
 library(leaflet)
 library(readr)
@@ -12,9 +27,12 @@ library(spatstat)
 library(tidyverse)
 library(raster)
 library(dismo)
-
-library(tidyverse)
 library(lubridate)
-library(raster)
 library(SDMtune)
-library(spatstat)
+
+# Load shapefile
+
+# Input occurrence data
+occdata <- read_csv("data/ATM_2023_0715-running-master.csv")
+
+occdata
