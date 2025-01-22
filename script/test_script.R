c0444c0efe91b3c5802477fddd40cb12f8a8981a
Vhon Garcia <81279614+grcvhon@users.noreply.github.com>
2025-01-22 15:39:07 +1030

    add functions and tidy up a bit

--- script/test_script.R
+++ script/test_script.R
@@ -1,7 +1,7 @@
 ### MaxEnt modelling framework ###
 ### 2025-01-16 ###
 
-### Install packages ---------------------------------------
+### i. Install packages ----------------------------------------------------------
 # install.packages("sf")
 # install.packages("leaflet")
 # install.packages("readr")
@@ -21,7 +21,7 @@
 # install.packages("ggspatial")
 # install.packages("prettymapr")
 
-### Load package list ------------------------------------------
+### ii. Load package list ---------------------------------------------------------
 packages <- c("sf","leaflet","readr","janitor","dplyr",
               "mapview","spatstat","tidyverse","raster",
               "dismo","lubridate","SDMtune","readxl",
@@ -29,15 +29,15 @@ packages <- c("sf","leaflet","readr","janitor","dplyr",
               "prettymapr")
 invisible(lapply(packages, library, character.only = TRUE))
 
-### Input model extent -----------------------------------------
+### 1. Input model extent -----------------------------------------------------
 
 # Load Northwest shelf bounding area
 nw_shelf <- st_read("data/shapefiles/nw-shelf/NWShelf.shp")
 mapview(nw_shelf)
 
-### Input environmental predictors -----------------------------
+### 2. Input environmental predictors -----------------------------------------
 
-# Load initial environmental predictors
+# Load initial set of environmental rasters
 env_init <- stack("data/predictor-variables/sal_mean.asc",
                   "data/predictor-variables/sal_amp.asc",
                   "data/predictor-variables/bathymetry.asc",
@@ -49,162 +49,87 @@ env_init <- stack("data/predictor-variables/sal_mean.asc",
                   "data/predictor-variables/DistToFW.asc")
 env_init
 
-### Input occurrence data --------------------------------------
+### 3. Input occurrence data --------------------------------------------------
 
-# Load occurrence data
-# csv contains combined occurrence data for 
+# Load occurrence data csv contains combined occurrence data for 
 # Aipysurus apraefrontalis and A. foliosquama
 rawdat <- read_csv("data/spreadsheets/ATM_2023_0715-running-master.csv")
 
-# filter for apraefrontalis
-aprae <- rawdat %>%
-  mutate(
-    lat = as.numeric(MidLat),
-    lat = if_else(is.na(lat), as.numeric(Latitude), lat),
-    long = as.numeric(MidLong),
-    long = if_else(is.na(long), as.numeric(Longitude), long),) %>%
-  filter(Species == "apraefrontalis",
-         !is.na(lat),
-         !is.na(long)) %>% 
-  clean_names()
+# function to filter by species
+filter_species <- function(species_name) {
+  rawdat %>%
+    mutate(
+      lat = as.numeric(MidLat),
+      lat = if_else(is.na(lat), as.numeric(Latitude), lat),
+      long = as.numeric(MidLong),
+      long = if_else(is.na(long), as.numeric(Longitude), long)
+    ) %>%
+    filter(Species == species_name,
+           !is.na(lat),
+           !is.na(long)) %>%
+    clean_names()
+}
 
-# filter for foliosquama
-folio <- rawdat %>%
-  mutate(
-    lat = as.numeric(MidLat),
-    lat = if_else(is.na(lat), as.numeric(Latitude), lat),
-    long = as.numeric(MidLong),
-    long = if_else(is.na(long), as.numeric(Longitude), long),) %>%
-  filter(Species == "foliosquama",
-         !is.na(lat),
-         !is.na(long)) %>% 
-  clean_names()
+# create data frame filtered for each species
+aprae <- filter_species("apraefrontalis")
+folio <- filter_species("foliosquama")
+
+# function to convert dataframe to simple feature (sf) 
+convert_2_sf <- function(df_name) {
+  df_name %>% 
+    st_as_sf(coords = c("long", "lat"), crs = 4326) %>% 
+    distinct(.keep_all = T)
+}
+
+# convert dataframe to simple feature (sf)
+aprae_sf <- convert_2_sf(aprae)
+folio_sf <- convert_2_sf(folio)
+
+### 3.1 Input transect data
+# shapefiles generated with "Generate and save shapefile 
+# using transect start and end coordinates.R"
+aprae_transect <- st_read("data/shapefiles/apraefrontalis_transect.shp")
+folio_transect <- st_read("data/shapefiles/foliosquama_transect.shp")
+
+### 4. Create Bias Layer ------------------------------------------------------
+# convert points and transect lines into a point pattern object (ppp or psp)
+aprae_occ_ppp <- aprae_sf %>% 
+  st_transform(crs = 3577) %>% 
+  as_Spatial() %>% 
+  maptools::as.ppp.SpatialPointsDataFrame(.)
+  
+aprae_trn_psp <- aprae_transect %>% 
+  st_transform(crs = 3577) %>% 
+  as_Spatial() %>% 
+  maptools::as.psp.SpatialLinesDataFrame(.)
+
+# calculate Gaussian density distribution of points and transects
+aprae_pts_bias <- aprae_occ_ppp %>% 
+  density(., sigma = 0.01) %>% 
+  raster()
+crs(aprae_pts_bias) <- CRS("+init=epsg:3577")
+aprae_pts_bias <- aprae_pts_bias %>% 
+  projectRaster(., crs = CRS("+init=epsg:4326")) %>% 
+  mask(mask = nw_shelf) %>% 
+  resample(x = ., y = env_init[[1]])
+values(aprae_pts_bias) <- values(aprae_pts_bias) + min(values(aprae_pts_bias), na.rm = T)
+
+aprae_trn_bias <- aprae_trn_psp %>% 
+  density(., sigma = 0.01) %>% 
+  raster()
+crs(aprae_trn_bias) <- CRS("+init=epsg:3577")
+aprae_trn_bias <- aprae_trn_bias %>% 
+  projectRaster(., crs = CRS("+init=epsg:4326")) %>% 
+  mask(mask = nw_shelf) %>% 
+  resample(x = ., y = env_init[[1]])
+values(aprae_trn_bias) <- values(aprae_trn_bias) + min(values(aprae_trn_bias), na.rm = T)
+
+aprae_bias_layer <- aprae_pts_bias + aprae_trn_bias
+aprae_bias_layer[values(aprae_bias_layer) < 0] <- NA
+plot(aprae_bias_layer)
+mapview(aprae_bias_layer) + aprae_sf
+
 
 # species-specific occurrence map + bounding area
 mapview(aprae, xcol = "long", ycol = "lat", crs = 4326) + nw_shelf  
 mapview(folio, xcol = "long", ycol = "lat", crs = 4326) + nw_shelf
-
-# convert to simple feature (sf) 
-aprae_sf <- aprae %>% 
-  st_as_sf(coords = c("long", "lat"), crs = 4326) %>% 
-  distinct(.keep_all = T)
-
-folio_sf <- folio %>% 
-  st_as_sf(coords = c("long", "lat"), crs = 4326) %>% 
-  distinct(.keep_all = T)
-
-### Create bias layer (for point observations) -----------------
-#### apraefrontalis ####
-aprae_ppp <- aprae_sf %>%
-  st_transform(crs = 3577) %>% 
-  as.ppp()
-
-ggplot() + 
-  annotation_map_tile("cartolight", zoom = 4) +
-  layer_spatial(aprae_sf, colour = "brown") +
-  layer_spatial(nw_shelf, fill = NA, col = "black", lwd = 0.3) +
-  annotation_scale(width_hint = 0.2, location = "br") +
-  theme_void()
-
-# Calculate Gaussian density distribution of points
-aprae_pts_bias <- aprae_ppp %>% 
-  density(., sigma = 0.05) %>% 
-  raster()
-
-crs(aprae_pts_bias) <- CRS("+init=epsg:3577")
-
-aprae_pts_bias <- aprae_pts_bias %>% 
-  projectRaster(., crs = CRS("+init=epsg:4326")) %>% 
-  mask(mask = nw_shelf) %>% 
-  resample(x = ., y = env_init[[1]]) # env_init are initial subset of environmental predictors - Line 40
-
-values(aprae_pts_bias) <- values(aprae_pts_bias) + min(values(aprae_pts_bias), na.rm = T)
-
-aprae_bias_layer <- aprae_pts_bias
-aprae_bias_layer[values(aprae_bias_layer) < 0] <- NA
-
-mapview(aprae_bias_layer)
-
-#### foliosquama ####
-folio_ppp <- folio_sf %>%
-  st_transform(crs = 3577) %>% 
-  as.ppp()
-
-ggplot() + 
-  annotation_map_tile("cartolight", zoom = 4) +
-  layer_spatial(folio_sf, colour = "brown") +
-  layer_spatial(nw_shelf, fill = NA, col = "black", lwd = 0.3) +
-  annotation_scale(width_hint = 0.2, location = "br") +
-  theme_void()
-
-# Calculate Gaussian density distribution of points
-folio_pts_bias <- folio_ppp %>% 
-  density(., sigma = 0.05) %>% 
-  raster()
-
-crs(folio_pts_bias) <- CRS("+init=epsg:3577")
-
-folio_pts_bias <- folio_pts_bias %>% 
-  projectRaster(., crs = CRS("+init=epsg:4326")) %>% 
-  mask(mask = nw_shelf) %>% 
-  resample(x = ., y = env_init[[1]]) # env_init are initial subset of environmental predictors - Line 40
-
-values(folio_pts_bias) <- values(folio_pts_bias) + min(values(folio_pts_bias), na.rm = T)
-
-folio_bias_layer <- folio_pts_bias
-folio_bias_layer[values(folio_bias_layer) < 0] <- NA
-
-mapview(folio_bias_layer)
-
-mapview(folio, xcol = "long", ycol = "lat", crs = 4326) + folio_bias_layer
-
-### Create bias layer (for transect lines) ---------------------
-
-sample <- folio %>% 
-  select(c(start_long,start_lat,end_long,end_lat)) %>% 
-  filter(apply(folio[, c("start_long", "start_lat", "end_long", "end_lat")], 1, function(x) all(!is.na(x))))
-
-# create an empty list to store transect line geometries
-trn_lines <- list()
-
-for(i in 1:nrow(sample)) {
-  start_gps <- c(sample$start_long[i], sample$start_lat[i])
-  end_gps <- c(sample$end_long[i], sample$end_lat[i])
-  
-  trn_gps <- rbind(start_gps, end_gps)
-  trn_line <- st_sfc(st_linestring(trn_gps), crs = 4326)
-  trn_lines[[i]] <- trn_line
-}
-
-trn_sf <- st_sf(geometry = do.call(c, trn_lines))
-mapview(trn_sf)
-
-### misc ------------------------------------------------------- 
-eez <- st_read("misc/from-LM/australia-eez/Australia_EEZ.shp")
-eez_crop <- st_crop(eez, 
-                    xmin = 106, xmax = 129, 
-                    ymin = -38, ymax = -10)
-mapview(eez_crop)
-
-bathymetry <- 
-  raster("data/predictor-variables/biooracle_bathy/bathymetry_mean_Layer.tif")
-mapview(bathymetry)
-
-class(eez_crop)
-class(bathymetry)
-
-eez_crop <- 
-  # project eez_crop with same projection as raster i.e., bathymetry
-  st_transform(eez_crop, crs(bathymetry)) 
-
-bathy_crop <- 
-  # crops only based on the x-min/max and y-min/max
-  crop(bathymetry, extent(eez_crop)) 
-
-bathy_mask <- 
-  # only area within the polygon is kept
-  mask(bathy_crop, eez_crop) 
-
-# compare
-mapview(bathy_crop)
-mapview(bathy_mask)
