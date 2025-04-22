5eebcd671f940f0b33056c5d8ed8074202b350c6
Vhon Garcia <81279614+grcvhon@users.noreply.github.com>
2025-04-22 16:36:44 +0930

    AFO-s3_7-refmapping

---
+++ genomics/scripts/04-ipyrad-s3_7-AFO-stringent-ref.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=AFO-stringent-s3_7
+#SBATCH -p icelake
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 20
+#SBATCH --time=4:00:00
+#SBATCH --mem=60GB
+#SBATCH -o /hpcfs/users/a1235304/atm/slurm/ipyrad-s37/%x_%j.log
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=vhon.garcia@adelaide.edu.au
+
+# Variables
+DIR='/hpcfs/users/a1235304/atm'
+cd "${DIR}/results/ipyrad" || exit 1
+
+#source "/home/a1645424/hpcfs/micromamba/etc/profile.d/micromamba.sh"
+#micromamba activate ipyrad
+
+ipyrad \
+    -s 34567 \
+    -p 'params-AFO-stringent-ref.txt' \
+    -c "${SLURM_CPUS_PER_TASK}"
+
+#micromamba deactivate
