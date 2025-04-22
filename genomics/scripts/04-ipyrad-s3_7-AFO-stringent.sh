08767d4736b70461bb53f8506f2a8c79bee0e174
Vhon Garcia <81279614+grcvhon@users.noreply.github.com>
2025-04-22 11:52:53 +0930

    04-ipyrad-s3_7-AFO-stringent

---
+++ genomics/scripts/04-ipyrad-s3_7-AFO-stringent.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=AFO-sringent-s3_7
+#SBATCH -p icelake
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 20
+#SBATCH --time=4:00:00
+#SBATCH --mem=60GB
+#SBATCH -o /hpcfs/users/a1235304/atm/slurm/ipyrad-s37/%x_%j.log
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=vhon.garcia@adelaide.edu.au
+
+# Variables
+DIR='/hpcfs/users/a1235304/atm'
+PARAMS="${DIR}/results/ipyrad/params"
+cd "${DIR}/results/ipyrad" || exit 1
+
+#source "/home/a1645424/hpcfs/micromamba/etc/profile.d/micromamba.sh"
+#micromamba activate ipyrad
+
+ipyrad \
+    -s 34567 \
+    -p "${PARAMS}/params-AFO-stringent.txt" \
+    -c "${SLURM_CPUS_PER_TASK}"
+
+#micromamba deactivate
