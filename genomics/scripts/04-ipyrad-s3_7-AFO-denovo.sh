e9ca5f6d895e79fcc4e202dbbd6bfc1f3273981c
Vhon Garcia <81279614+grcvhon@users.noreply.github.com>
2025-04-24 16:39:07 +0930

    AFO-denovo script

--- genomics/scripts/04-ipyrad-s3_7-AFO-denovo.sh
+++ genomics/scripts/04-ipyrad-s3_7-AFO-denovo.sh
@@ -3,8 +3,8 @@
 #SBATCH -p icelake
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -c 20
-#SBATCH --time=4:00:00
+#SBATCH -c 8
+#SBATCH --time=1:00:00
 #SBATCH --mem=60GB
 #SBATCH -o /hpcfs/users/a1235304/atm/slurm/ipyrad-s37/%x_%j.log
 #SBATCH --mail-type=FAIL
@@ -16,10 +16,16 @@ cd "${DIR}/results/ipyrad" || exit 1
 
 #source "/home/a1645424/hpcfs/micromamba/etc/profile.d/micromamba.sh"
 #micromamba activate ipyrad
+
+source "/gpfs/apps/icl/software/Anaconda3/2024.06-1/etc/profile.d/conda.sh"
+
 conda activate ipyrad
+
 ipyrad \
     -s 34567 \
     -p 'params-AFO-denovo.txt' \
     -c "${SLURM_CPUS_PER_TASK}"
+
 conda deactivate
+
 #micromamba deactivate
