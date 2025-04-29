2b73b6e71bd5a253174977ac19cdd33fe7f980f5
Vhon Garcia <81279614+grcvhon@users.noreply.github.com>
2025-04-29 15:54:39 +0930

    test py conv

--- genomics/scripts/06-vcf2hdf5.py
+++ genomics/scripts/06-vcf2hdf5.py
@@ -2,10 +2,18 @@ import ipyrad.analysis as ipa
 
 # Aipysurus foliosquama
 
-## Unfiltered ipyrad output
+## AFO - reference - Unfiltered ipyrad output
 converter = ipa.vcf_to_hdf5(
     name="AFO-reference.LD50k",
     data="/hpcfs/users/a1235304/atm/results/ipyrad/AFO-reference_outfiles/AFO-reference.vcf.gz",
+    workdir='/hpcfs/users/a1235304/atm/results/ipyrad/AFO-reference_outfiles/',
+    ld_block_size=50000
+)
+
+## AFO - reference - Filtered ipyrad output
+converter = ipa.vcf_to_hdf5(
+    name="AFO-reference.highQ.filtered.LD50k",
+    data="/hpcfs/users/a1235304/atm/results/ipyrad/AFO-reference_outfiles/AFO-reference.highQ.filtered.vcf.gz",
     workdir='/hpcfs/users/a1235304/atm/results/ipyrad/AFO-reference_outfiles/',
     ld_block_size=50000
 )
