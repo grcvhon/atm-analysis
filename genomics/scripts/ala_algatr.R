f1d70d28376480bbcbd938836a665cfe1f93a723
Vhon Garcia <81279614+grcvhon@users.noreply.github.com>
2025-06-05 16:48:03 +0930

    line 156 - note on extent

--- genomics/scripts/ala_algatr.R
+++ genomics/scripts/ala_algatr.R
@@ -90,13 +90,14 @@ bound <- extent(c(xmin = 111.9903,
                   ymax = -11.1))
 mspec_raster <- crop(mspec_layers, bound)
 plot(mspec_raster)
+plot(mspec_raster[[1]])
 points(laevis_nw_proj)
 
 library(RStoolbox)
 mspec_pcs <- rasterPCA(mspec_raster, spca = TRUE)
 mspec_pcs
 # this mspec_stack, same class as `CA_env` in accompanying example
-mspec_stack <- raster::stack(mspec_pcs$map) 
+mspec_stack <- raster::stack(mspec_pcs$map)
 
 # TESS
 laevis_tess <- tess_ktest(laevis_dosage, laevis_nw_proj, Kvals = 1:7, ploidy = 2, K_selection = "auto")
@@ -112,4 +113,46 @@ laevis_qmat <- tess3r::qmatrix(laevis_tessobj, K = laevis_bestK)
 # tess3 krig raster
 mspec_krig <- (mspec_stack[[1]]) # no aggregate factor
 
+#mspec_krig1 <- projectRaster(mspec_krig, crs = "epsg:4326")
+
 laevis_krig_admix <- tess_krig(laevis_qmat, laevis_nw_proj, mspec_krig)
+
+## this bit run...
+x <- st_as_sf(laevis_nw_coords, coords = c("x", "y"), crs = 4326)
+x_proj <- st_transform(x, crs = 3112)
+y_krig_raster <- projectRaster(mspec_krig, crs = "epsg:3112")
+
+crs(x_proj)
+crs(y_krig_raster)
+
+z_krig_admix <- tess_krig(laevis_qmat, x_proj, y_krig_raster)
+
+# TESS
+tess_ggplot(z_krig_admix, 
+            plot_method = "maxQ", 
+            #ggplot_fill = scale_fill_manual(values = c("#bd9dac", "#257b94", "#476e9e")),
+            plot_axes = TRUE, 
+            coords = x_proj)
+
+# wingen
+laevis_lyr <- coords_to_raster(x_proj, res = 10000, buffer = 5, plot = TRUE)
+ggplot_count(preview_gd(laevis_lyr, x_proj, wdim = 5, fact = 6))
+l_gd <- window_gd(laevis_vcf, x_proj, laevis_lyr, stat = "pi", wdim = 5, fact = 6)
+plot(l_gd[[1]])
+points(x_proj)
+#ggplot_gd(l_gd)
+
+l_kgd <- krig_gd(l_gd, index = 1:2, laevis_lyr) # krig genetic diversity; about 2 hours?; increase wdim and fact = quicker
+plot(l_kgd[[1]])
+points(x_proj)
+#ggplot_count(l_kgd)
+
+l_mgd_1 <- mask_gd(l_kgd, l_kgd[["pi"]], minval = 0.0001)
+plot(l_mgd_1[[1]])
+points(x_proj)
+ggplot_gd(l_mgd_1, bkg = y_krig_raster)
+
+mapview::mapview(x_proj) + mapview::mapview(l_kgd[[1]]) 
+# pi showing and interpolated throughout laevis_nw_proj extent ... need to modify extent ... use extent of bathymetry.asc?
+
+## End
