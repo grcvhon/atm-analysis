28cc4d23625a77a8179750da2582ff14f2fbde6c
Vhon Garcia <81279614+grcvhon@users.noreply.github.com>
2025-06-06 16:17:32 +0930

    init commit

--- genomics/scripts/alaevis.R
+++ genomics/scripts/alaevis.R
@@ -1,8 +1,109 @@
+# alaevis.R
+# 06 June 2025 - combined from other script files
 
+# Prepare sample list general - A laevis NW shelf samples ----
+library(tidyverse)
+# Load csv
+laevis_popmap <- 
+  read.csv("./genomics/ALA-popmap-coords.csv", 
+           sep = ",", 
+           header = TRUE)
 
+laevis_nw <- laevis_popmap %>% 
+  # Remove AL401 and AL404 - dropped samples
+  subset(id!="AL401") %>% 
+  subset(id!="AL404") %>% 
+  # Keep only samples west of GoC
+  subset(pop!="North_QLD") %>% 
+  subset(pop!="Gulf_of_Carpentaria") %>% 
+  subset(pop!="New_Caledonia")
 
-# TESS
-# STRUCTURE-like analyses
+laevis_nw <- laevis_nw %>%
+  # unite first 3 column values separated with "-"
+  unite("id_clean", species:targetid, remove = FALSE, sep = "-") %>% 
+  # arrange columns as desired
+  select("id_clean","pop","locality","longitude","latitude")
+laevis_nw
+
+# create keep list for vcf
+laevis_vcf_keep <- laevis_nw %>% 
+  select("id_clean")
+# remove header from keep list
+colnames(laevis_vcf_keep) <- NULL
+write.table(laevis_vcf_keep,
+            file = "./genomics/ALA-nw.txt",
+            sep = ",",
+            row.names = FALSE,
+            quote = FALSE)
+
+# Genetic data processing - loading A laevis DArTseq VCF ----
+
+# after filtering VCF via "Filter by individual" (i.e., use of keep list)
+# load vcf file
+laevis_vcf <- 
+  vcfR::read.vcfR("./genomics/vcf_files/ALA-stringent.highQ.filtered.nw.keep.vcf", 
+                  verbose = TRUE)
+
+laevis_dosage <- 
+  wingen::vcf_to_dosage(laevis_vcf)
+
+laevis_dos_imp <- 
+  algatr::simple_impute(laevis_dosage, FUN = median)
+
+# Prepare sample list for algatr analyses ----
+
+# obtain list of samples from VCF
+laevis_samples <- as.data.frame(colnames(laevis_vcf@gt)) # get sample names from ala_vcf
+laevis_samples <- as.data.frame(laevis_samples[-1,]) # remove "FORMAT" colname
+colnames(laevis_samples) <- "sample" # rename "FORMAT" as "sample"
+laevis_samples
+
+laevis_nw_coords <- laevis_nw %>%
+  # arrange `laevis_nw_coords` as in samples in vcf file
+  arrange(id_clean, laevis_samples$sample) %>% 
+  # select only long and lat cols
+  select("longitude","latitude")
+colnames(laevis_nw_coords) <- c("x","y") # rename cols
+
+laevis_nw_proj <- sf::st_as_sf(laevis_nw_coords, coords = c("x", "y"), crs = "epsg:4326")
+laevis_nw_proj <- sf::st_transform(laevis_nw_proj, crs = 4326)
+
+# MARSPEC - get marine layer datasets ----
+
+library(sdmpredictors)
+library(raster)
+library(sp)
+library(dismo)
+
+# MARSPEC data sets
+# list names of MARSPEC layers (annual data) 
+mspec_names <- list_layers("MARSPEC", monthly = FALSE)$name 
+# all annual layers
+mspec_annual <- list_layers("MARSPEC", monthly = FALSE)$layer_code 
+# download the layers
+mspec_layers <- load_layers(mspec_annual) 
+
+# From `./sdm/scripts/maxent-model-fw.R`
+# nw_shelf <- st_read("data/shapefiles/nw-shelf/NWShelf.shp", quiet = TRUE) %>% st_transform(4326)
+# mapview::mapview(nw_shelf)
+# effort_nwshelf_sf <- mask(effort_vect, mask = vect(nw_shelf))
+
+mspec_spatrast <- raster::crop(mspec_layers, nw_shelf)
+mspec_spatrast <- terra::rast(mspec_spatrast) # global; convert RasterBrick to SpatRaster
+mspec_shelf <- terra::mask(mspec_spatrast, mask = terra::vect(nw_shelf))
+plot(mspec_shelf[[1]])
+points(laevis_nw_coords)
+
+mspec_sh_pcs <- RStoolbox::rasterPCA(mspec_shelf, spca = TRUE)
+mspec_sh_stack <- raster::stack(mspec_sh_pcs$map)
+mspec_sh_stack
+
+# TESS + Kriging - interpolate ancestry coefficients across the land(sea)scape ----
+
+# Notes:
+# > In tess_ktest(), best K value changes every iteration (K_selection = "auto").
+# > When K = 2, both Ks are represented on the plot. When K = 3, only K1 shows up. 
+
 laevis_tess <- 
   algatr::tess_ktest(gen = laevis_dosage, 
                      coords = laevis_nw_proj, 
@@ -21,9 +122,12 @@ laevis_qmat <-
                   K = laevis_bestK)
 
 # Prepare for kriging
+
+# grid
 mspec_sh_krig <- (mspec_sh_stack[[1]]) # environmental PC1, nw shape
 y_krig_sh_raster <- raster::projectRaster(mspec_sh_krig, crs = "epsg:3112")
 
+# sample coordinates
 x <- sf::st_as_sf(laevis_nw_coords, coords = c("x", "y"), crs = 4326)
 x_proj <- sf::st_transform(x, crs = 3112) # EPSG:3112; GDA94 Geoscience Australia projection 
 
