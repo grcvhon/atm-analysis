b9f5f0428f2201f2fb570d0da1750fabbe9a4a1e
Vhon Garcia <81279614+grcvhon@users.noreply.github.com>
2025-06-12 13:39:27 +0930

    init commit

---
+++ genomics/circuitscape/circuitscape_in_Julia.jl
@@ -0,0 +1,8 @@
+# Following https://github.com/Circuitscape/Circuitscape.jl
+
+# Circuitscape.jl installation
+using Pkg
+Pkg.add("Circuitscape")
+
+# Check if all the tests are passing by doing the following:
+julia> Pkg.test("Circuitscape")
