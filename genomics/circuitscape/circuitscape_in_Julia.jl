409ea36afa2ecfe32121e37766f4a414d95148fc
Vhon Garcia <81279614+grcvhon@users.noreply.github.com>
2025-06-13 13:28:00 +0930

    update commit

--- genomics/circuitscape/circuitscape_in_Julia.jl
+++ genomics/circuitscape/circuitscape_in_Julia.jl
@@ -1,3 +1,24 @@
+# Omniscape X Circuitscape
+# Created 12 June 2025
+# Vhon Garcia
+
+# This is also my first time using Julia lang so I have notes below
+# about basic commands
+# - pwd() print present working dir
+# - cd("path/to/dir") change working dir
+# - plot() ... capable of plotting, will open new window to print plot
+
+#################
+# Omniscape
+#################
+# Following https://docs.circuitscape.org/Omniscape.jl/latest/usage/
+# Circuitscape.jl installation
+using Pkg
+Pkg.add("Omniscape")
+
+#################
+# Circuitscape
+#################
 # Following https://github.com/Circuitscape/Circuitscape.jl
 
 # Circuitscape.jl installation
